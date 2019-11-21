b65b63ddf27af34e79eea24f525beab1f91fc6d6
Akhat <akhatnya@gmail.com>
2019-11-21 13:41:42 +0600

    rest api

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const express = require('express');
+const app = express();
+
 const { Pool } = require('pg');
 
 const connection = new Pool({
@@ -8,14 +11,59 @@ const connection = new Pool({
     port: 5432,
 });
 
-connection.query("select * from todosasd", 
-    (err, res) => {
 
-        if (err) {
-            console.error(err);
-        } else {
-            console.log(res.rows);
+
+app.get(
+    "/getSomeData",
+    (request, response) => {
+
+        connection.query("select * from todos", 
+        (err, res) => {
+
+            if (err) {
+                console.error(err);
+            } else {
+                response.json(res.rows);
+            }
+        });
+
+    }
+);
+
+app.get(
+    "/deleteById/:id",
+    (request, response) => {
+
+        connection.query("delete from todos where id = $1",
+        [
+            request.params.id
+        ],
+        (err, res) => {
+
+            if (err) {
+                response.json({ msg: err });
+            } else {
+                response.json({ msg: "successfully deleted" });
+            }
+        });
+
+    }
+);
+
+app.get("/create/:desciption", (request, response) => {
+
+    connection.query(
+        "insert into todos(desciption) values($1)",
+        [ request.params.desciption ],
+        (err, res) => {
+            if (err) {
+                response.json({ msg: err });
+            } else {
+                response.json({ msg: "successfully inserted !" });
+            }
         }
+        );
 
-        connection.end();
-    });
+});
+
+app.listen(8080, () => { console.log("Приложуха запустилась"); });
